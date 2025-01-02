88cda215edf1bd86129a10034a8e40bd3008e036
Satria-WS <extencezone02@gmail.com>
2025-01-02 17:19:39 +0700

    adding accest token service

--- backend/services/tokenService.js
+++ backend/services/tokenService.js
@@ -9,17 +9,16 @@ export const generateAccessToken = (userId, name, email) => {
 
 //create refresh token
 export const generateRefreshToken = (userId, name, email) => {
-  return (jwt.sign({ userId, name, email }, process.env.REFRESH_TOKEN_SECRET),{
-    expiresIn: "1d",
-    });
+  return jwt.sign({ userId, name, email }, process.env.REFRESH_TOKEN_SECRET, {
+    expiresIn:"1d"
+  })
 };
 
 //function for manage refresh Token in cookies
-
 export const setRefreshTokenCookie = (res, refreshToken) => {
   res.cookie("refreshToken", refreshToken, {
     httpOnly: true,
     maxAge: 24 * 60 * 60 * 1000, // 1 day
     // secure: true, // Aktifkan jika menggunakan HTTPS
-  })
-}
+  });
+};
