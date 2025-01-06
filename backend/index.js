028dd9edadb402dfcb07ef58cc7de4e974c03138
Satria-WS <extencezone02@gmail.com>
2025-01-06 10:04:34 +0700

    add cookie-parser middleware for handling cookies

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import db from './config/Database.js'; // Impor konfigurasi database.
 import Users from './models/UserModel.js'; // Impor model tabel "Users".
 import router from './routes/index.js'; // Impor rute-rute yang kamu buat.
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 const app = express();
@@ -14,9 +15,13 @@ try {
 } catch (error) {
   console.log(error);
 }
+//app-cooker-parser
+app.use(cookieParser());
 // Menambahkan middleware supaya server bisa menerima dan membaca data dalam format JSON dari permintaan (request) yang dikirim user.
 app.use(express.json());
 // Mengatur semua rute atau jalan ke berbagai "halaman" server.
 app.use(router);
+
+
 
 app.listen(5000, () => console.log('server running at port 5000'));
