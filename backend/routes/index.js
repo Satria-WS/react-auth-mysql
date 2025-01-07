0884340056f6dada9492dbe2d377e671b8e130de
Satria-WS <extencezone02@gmail.com>
2025-01-07 15:47:03 +0700

    add delete user by ID route in Users controller

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,11 +1,18 @@
 import express from 'express';
-import { getUsers, Login, Logout, Register } from '../controllers/Users.js';
+import {
+  deleteUserById,
+  getUsers,
+  Login,
+  Logout,
+  Register,
+} from '../controllers/Users.js';
 import { verifyToken } from '../middleware/VerifyToken.js';
 import { refreshToken } from '../controllers/RefreshToken.js';
 
 const router = express.Router();
 router.get('/users', verifyToken, getUsers);
 router.post('/users', Register);
+router.delete('/users/:id', verifyToken, deleteUserById);
 router.post('/login', Login);
 router.get('/token', refreshToken);
 router.delete('/logout', Logout);
