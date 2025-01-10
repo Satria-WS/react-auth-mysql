8cccc984896d82ab2b26b490aabc357f01f99923
Satria-WS <extencezone02@gmail.com>
2025-01-10 21:35:30 +0700

    remove commented-out code from refreshToken controller

--- backend/controllers/RefreshToken.js
+++ backend/controllers/RefreshToken.js
@@ -53,3 +53,42 @@ export const refreshToken = async (req, res) => {
     res.status(500).json({ message: 'Internal server error' });
   }
 };
+
+// export const refreshToken = async (req, res) => {
+//   try {
+//     const refreshToken = req.cookies.refreshToken;
+//     if (!refreshToken) {
+//       return res.status(401);
+//     }
+//     const user = await Users.findAll({
+//       where: {
+//         refresh_token: refreshToken,
+//       },
+//     });
+//     if (!user[0]) {
+//       return res.status(403);
+//     }
+//     jwt.verify(
+//       refreshToken,
+//       process.env.REFRESH_TOKEN_SECRET, (err, decoded) => {
+//         if (err) {
+//           return res.status(403).send('Invalid or expired refresh token.');
+//         }
+
+//         const userId = user[0].id;
+//         const name = user[0].name;
+//         const email = user[0].email;
+
+//         const accessToken = jwt.sign(
+//           { userId, name, email },
+//           process.env.ACCESS_TOKEN_SECRET,
+//           { expiresIn: '15s' },
+//         );
+
+//         return res.json({ accessToken });
+//       },
+//     );
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
