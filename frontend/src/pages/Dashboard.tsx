a10c608d7adcddb3d919a6543519b36fed1ab10c
Satria-WS <extencezone02@gmail.com>
2025-01-10 18:51:03 +0700

    add token refresh functionality to Dashboard component

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,6 +1,27 @@
-import React from "react"
+import React , {useState,useEffect} from "react"
+import axios from 'axios';
+import { jwtDecode } from "jwt-decode";
+
 
 const Dashboard = () => {
+  const [name, setName] = useState('');
+  const [token, setToken] = useState('');
+
+  useEffect(() => {
+    refreshToken();
+  },[])
+
+  const refreshToken = async () => {
+    try {
+      const response = await axios.get('http://localhost:5000/token')
+      setToken(response.data.accessToken)
+      const decoded = jwtDecode(response.data.accessToken)
+      console.log(decoded);
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
     <div>Dashboard</div>
   )
